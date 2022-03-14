bfd3cd4407a138ccc08f635777e1768ae2652078
karthik <shetkarthik89@gmail.com>
2022-03-14 17:47:20 +0530

    adding

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const hbs = require("hbs");
-const port = process.nextTick.PORT || 8000
+const port = process.env.PORT || 8000
 
 const staticPath = path.join(__dirname,"../public")
 const templatePath = path.join(__dirname,"../templates/views");
